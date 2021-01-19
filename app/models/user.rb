64d8e3e5b7756e198db26b8d21dedf77db0afd5d
kei-sukey <rough1diamond99k@gmail.com>
2021-01-19 13:37:38 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+
+  validates :password,        format: { with: PASSWORD_REGEX,
+                                        message: 'Include both letters and numbers' }
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/,
+                                    message: 'Full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/,
+                                     message: 'Full-width characters' }
+    validates :last_kana_name, format: { with: /\A[ァ-ヶー－]+\z/,
+                                         message: 'Full-width katakana characters' }
+    validates :first_kana_name, format: { with: /\A[ァ-ヶー－]+\z/,
+                                          message: 'Full-width katakana characters' }
+    validates :birthday
+  end
+end
