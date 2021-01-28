9d773dce63454596afe813fde01e509c3bcfc640
kei-sukey <rough1diamond99k@gmail.com>
2021-01-28 18:30:01 +0900

    コメントアウト削除

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -3,23 +3,19 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
     const ImageList = document.getElementById('image-list');
 
     const createImageHTML = (blob) => {
-       // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
 
-      // 表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
 
       const prevImage = document.getElementById('image-list');
       prevImage.classList.add('prev_img');
 
-      // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
     };
 
     document.getElementById('item_image').addEventListener('change', function(e){
-      // 画像が表示されている場合のみ、すでに存在している画像を削除する
       const imageContent = document.querySelector('prev_img');
       if (imageContent){
         imageContent.remove();
